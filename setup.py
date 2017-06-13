f25324a1202c7897197ebc3b95d94b8e49f98db0
zaeval <h3065@naver.com>
2017-06-14 01:40:39 +0900

    first_commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ccfs',
+  packages = ['ccfs'], # this must be the same as the name above
+  version = '0.2',
+  description = 'count character from string lib',
+  author = 'zaeval',
+  author_email = 'zaeval@kookmin.ac.kr',
+  url = 'https://github.com/zaeval/ccfs', # use the URL to the github repo
+  download_url = 'https://github.com/zaeval/count_character_from_string/tree/master/ccfs/dist/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['count','character','string'], # arbitrary keywords
+  classifiers = [],
+)
